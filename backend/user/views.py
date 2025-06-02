1aa84a8840f86300d0aa2cd366927385d5037335
Julia Davydova <132129149+pishnik212@users.noreply.github.com>
2025-06-02 14:47:22 +0500

    Update views.py

--- backend/user/views.py
+++ backend/user/views.py
@@ -20,7 +20,6 @@ def login_view(request):
                 'email': user.email,
                 'first_name': user.first_name,
                 'last_name': user.last_name,
-                # добавь что нужно
             },
             'success': True
         })
